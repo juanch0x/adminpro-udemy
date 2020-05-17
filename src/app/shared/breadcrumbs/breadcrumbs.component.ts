2e9b812740e9ae280e6770e7c6ecf2d40dfd2314
Juan Portugal <juan.portugal.ext@bayer.com>
2020-05-17 15:38:47 -0300

    Sección 8 terminada

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivationEnd } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
+import { Observable } from 'rxjs/Observable';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-breadcrumbs',
   templateUrl: './breadcrumbs.component.html',
-  styles: []
+  styles: [],
 })
 export class BreadcrumbsComponent implements OnInit {
+  titulo: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private _router: Router, private _title : Title) {
+    this.getDataRoute().subscribe((x) => {
+      this.titulo = x.titulo;
+      this._title.setTitle(this.titulo);
+    });
   }
 
+  ngOnInit() {}
+
+  getDataRoute(): Observable<any> {
+    return this._router.events.pipe(
+      filter((x) => x instanceof ActivationEnd),
+      filter((x: ActivationEnd) => x.snapshot.firstChild === null),
+      map((x: ActivationEnd) => x.snapshot.data)
+    );
+  }
 }
